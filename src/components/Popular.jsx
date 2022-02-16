262e7424d916a1f89044a1d82b587e2d224bf29e
jlmoer <jlmoerdijk@gmail.com>
2022-02-16 17:08:19 +0100

    Moovice Part 3

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 
 class Popular extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            movies: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4cc1a3680987cfb0c255bd90ac481588")
+            .then((res) => res.json())
+            .then((res) => {
+                this.setState({ movies: res.results })
+            });
+    }
+
     render() {
         return (
             <div>
